434e3e323b322b1f136824988de249fff8374eb2
Jan Patrick Maureal <janpatrick@ishare.com.ph>
2024-03-20 16:19:40 +0800

    add link assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,46 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+		<title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+		<!-- Fonts -->
+		<link href="https://fonts.gstatic.com" rel="preconnect">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+		<!-- Vendor CSS Files -->
+		<link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+		<link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+		<link href="{{ asset('vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+		<link href="{{ asset('vendor/quill/quill.snow.css') }}" rel="stylesheet">
+		<link href="{{ asset('vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+		<link href="{{ asset('vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+		<link href="{{ asset('vendor/simple-datatables/style.css') }}" rel="stylesheet">
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+		<!-- Template Main CSS File -->
+		<link href="{{ asset('css/theme/admin/style.css') }}" rel="stylesheet">
+		<!-- Scripts -->
+		@vite(['resources/css/app.css', 'resources/js/app.js'])
+	</head>
+	<body>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+		<main id="main" class="main">
+			{{ $slot }}
+		</main>
+
+		<script src="{{ asset('vendor/apexcharts/apexcharts.min.js') }}"></script>
+		<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+		<script src="{{ asset('vendor/chart.js/chart.umd.js') }}"></script>
+		<script src="{{ asset('vendor/echarts/echarts.min.js') }}"></script>
+		<script src="{{ asset('vendor/quill/quill.min.js') }}"></script>
+		<script src="{{ asset('vendor/simple-datatables/simple-datatables.js') }}"></script>
+		<script src="{{ asset('vendor/tinymce/tinymce.min.js') }}"></script>
+		<script src="{{ asset('vendor/php-email-form/validate.js') }}"></script>
+
+		<!-- Template Main JS File -->
+		<script src="{{ asset('js/theme/admin/main.js') }}"></script>
+	</body>
 </html>
