d75d42ba59e480f264796ed89a0a028ba8cb7686
Jan Patrick Maureal <janpatrick@ishare.com.ph>
2024-03-20 16:41:53 +0800

    add dashboard layout

--- resources/views/layouts/partials/admin/navbar.blade.php
+++ resources/views/layouts/partials/admin/navbar.blade.php
@@ -3,7 +3,7 @@
 
 	<div class="d-flex align-items-center justify-content-between">
 		<a href="index.html" class="logo d-flex align-items-center">
-			<img src="assets/img/logo.png" alt="">
+			<img src="{{ asset('img/logo.png') }}" alt="">
 			<span class="d-none d-lg-block">NiceAdmin</span>
 		</a>
 		<i class="bi bi-list toggle-sidebar-btn"></i>
@@ -35,7 +35,7 @@
 							<span>Account Settings</span>
 						</a>
 					</li>
-					
+
 					<li>
 						<hr class="dropdown-divider">
 					</li>
