58cb13d8d3c4362e01b4d080063ddfa08ca9a8d4
Jan Patrick Maureal <janpatrick@ishare.com.ph>
2024-03-21 10:43:27 +0800

    add edit profile

--- resources/views/layouts/partials/admin/navbar.blade.php
+++ resources/views/layouts/partials/admin/navbar.blade.php
@@ -30,7 +30,7 @@
 					</li>
 
 					<li>
-						<a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+						<a class="dropdown-item d-flex align-items-center" href="{{ route('profile.edit') }}">
 							<i class="bi bi-gear"></i>
 							<span>Account Settings</span>
 						</a>
