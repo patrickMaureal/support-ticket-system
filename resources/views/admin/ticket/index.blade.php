1154381819f1dcab1bdf1bb752f2cd3e4bac1e88
Jan Patrick Maureal <janpatrick@ishare.com.ph>
2024-03-21 15:04:22 +0800

    update index

--- resources/views/admin/ticket/index.blade.php
+++ resources/views/admin/ticket/index.blade.php
@@ -1,2 +1,8 @@
 <x-app-layout>
+	<div class="pagetitle mb-4">
+		<h1>Tickets</h1>
+	</div><!-- End Page Title -->
+
+	<button type="button" class="btn btn-primary"><i class="bi bi-ticket me-1"></i>Add Ticket</button>
+
 </x-app-layout>
