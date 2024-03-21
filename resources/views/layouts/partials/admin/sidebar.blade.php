b0ffd6659d825b91c2fb9578a852ac86e9b08e98
Jan Patrick Maureal <janpatrick@ishare.com.ph>
2024-03-21 13:12:22 +0800

    add ticket route

--- resources/views/layouts/partials/admin/sidebar.blade.php
+++ resources/views/layouts/partials/admin/sidebar.blade.php
@@ -3,19 +3,19 @@
 
 	<ul class="sidebar-nav" id="sidebar-nav">
 
-		<li class="nav-item {{ request()->routeIs('dashboard') ? 'active' : '' }}">
-			<a class="nav-link" href="{{ route('dashboard') }}">
+		<li class="nav-item">
+			<a class="nav-link {{ request()->routeIs('dashboard') ? '' : 'collapsed' }}" href="{{ route('dashboard') }}">
 				<i class="bi bi-grid"></i>
 				<span>Dashboard</span>
 			</a>
 		</li><!-- End Dashboard Nav -->
 
-		<li class="nav-item {{ request()->routeIs('tickets.*') ? 'active' : '' }}">
-			<a class="nav-link " href="{{ route('tickets.index') }}">
+		<li class="nav-item">
+			<a class="nav-link {{ request()->routeIs('tickets.*') ? '' : 'collapsed' }}" href="{{ route('tickets.index') }}">
 				<i class="bi bi-ticket-perforated"></i>
 				<span>Tickets</span>
 			</a>
-		</li><!-- End Ticket Nav -->
+		</li><!-- End Error 404 Page Nav -->
 
 	</ul>
 
