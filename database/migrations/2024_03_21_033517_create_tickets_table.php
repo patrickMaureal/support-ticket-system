dcd1e24b47ddc5dc0cb9f9249eff0027849cfde6
Jan Patrick Maureal <janpatrick@ishare.com.ph>
2024-03-25 11:16:32 +0800

    update default value in migration

--- database/migrations/2024_03_21_033517_create_tickets_table.php
+++ database/migrations/2024_03_21_033517_create_tickets_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
 			$table->string('title');
 			$table->text('description');
 			$table->string('priority');
-			$table->string('status')->default('open');
+			$table->string('status')->default('Open');
 			$table->timestamps();
 			$table->softDeletes();
 		});
