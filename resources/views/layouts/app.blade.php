a693806191d6289f4a53acb2a53be0f638d57fbb
Jan Patrick Maureal <janpatrick@ishare.com.ph>
2024-03-25 12:45:38 +0800

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,9 @@
 
 		<title>{{ config('app.name', 'Laravel') }}</title>
 
+		<link href="{{ asset('img/favicon.png') }}" rel="icon">
+		<link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
 		<!-- Fonts -->
 		<link href="https://fonts.gstatic.com" rel="preconnect">
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
