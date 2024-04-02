794b987f202d39e5425450f249b343c56829c2b1
Jan Patrick Maureal <janpatrick@ishare.com.ph>
2024-04-02 11:37:12 +0800

    update user migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,12 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@test.com',
-            'password' => '12345678',
-        ]);
+			$this->call([
+				UserSeeder::class,
+			]);
     }
 }
