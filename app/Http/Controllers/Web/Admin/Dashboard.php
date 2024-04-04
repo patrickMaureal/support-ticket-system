2efd3605c19a293f5af705830198b0a905e5e6af
Jan Patrick Maureal <janpatrick@ishare.com.ph>
2024-04-04 14:13:00 +0800

    update dashboard count

--- app/Http/Controllers/Web/Admin/Dashboard.php
+++ app/Http/Controllers/Web/Admin/Dashboard.php
@@ -13,6 +13,26 @@ class Dashboard extends Controller
 	 */
 	public function __invoke(Ticket $ticket, Request $request)
 	{
-		return view('dashboard',compact('ticket'));
+
+		$user = auth()->user();
+		// Check if the authenticated user is an admin
+		if ($user->hasRole('Administrator')) {
+		// If admin, count all tickets
+		$ticketcount = Ticket::count();
+		$openticket = Ticket::where('status', 'Open')->count();
+		$closedticket = Ticket::where('status', 'Closed')->count();
+		} else {
+			// If not admin, count tickets created by the user or assigned to them
+			$ticketcount = Ticket::where(function ($query) {
+					$query->where('created_at', auth()->id()) // Tickets created by the authenticated user
+							->orWhere('agent', auth()->id()); // Tickets assigned to the authenticated user
+			})->count();
+			$openticket = Ticket::where('status', 'Open')->where('agent', auth()->id())->count();
+			$closedticket = Ticket::where('status', 'Closed')->where('agent', auth()->id())->count();
+		}
+
+
+
+		return view('dashboard', compact('ticketcount', 'openticket', 'closedticket'));
 	}
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,14 +22,47 @@
 										<i class="bi bi-ticket"></i>
 									</div>
 									<div class="ps-3">
-										<span class="text-muted small pt-2 ps-1 fw-bold">Total Tickets</span>
-										<h6 class="fw-bold">{{ $ticket->count() }}</h6>
+										<h6 class="fw-bold">{{ $ticketcount }}</h6>
 									</div>
 								</div>
 							</div>
 
 						</div>
-					</div><!-- End Sales Card -->
+					</div><!-- End Ticket Card -->
+					<div class="col-md-4">
+						<div class="card info-card revenue-card">
+							<div class="card-body">
+								<h5 class="card-title">Open Tickets</h5>
+
+								<div class="d-flex align-items-center">
+									<div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+										<i class="bi bi-ticket"></i>
+									</div>
+									<div class="ps-3">
+										<h6 class="fw-bold">{{ $openticket }}</h6>
+									</div>
+								</div>
+							</div>
+
+						</div>
+					</div><!-- End Open Ticket Card -->
+					<div class="col-md-4">
+						<div class="card info-card customers-card">
+							<div class="card-body">
+								<h5 class="card-title">Closed Tickets</h5>
+
+								<div class="d-flex align-items-center">
+									<div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+										<i class="bi bi-ticket"></i>
+									</div>
+									<div class="ps-3">
+										<h6 class="fw-bold">{{ $closedticket }}</h6>
+									</div>
+								</div>
+							</div>
+
+						</div>
+					</div><!-- End Closed Ticket Card -->
 
 				</div>
 			</div><!-- End Left side columns -->
