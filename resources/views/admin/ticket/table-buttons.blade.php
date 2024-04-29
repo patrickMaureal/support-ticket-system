867ea94a69b6a824f55f3a016db943a7ac93a2eb
patrickMaureal <janpatrickmaureal@gmail.com>
2024-04-29 11:58:50 +0800

    add role validation

--- resources/views/admin/ticket/table-buttons.blade.php
+++ resources/views/admin/ticket/table-buttons.blade.php
@@ -1,4 +1,6 @@
 <div>
 	<a class="btn btn-success btn-sm" href="{{ route('tickets.edit', $id) }}"><i class="bi bi-pen"></i></a>
-	<button class="delete-ticket btn btn-danger btn-sm" data-id="{{ $id }}"><i class="bi bi-trash"></i></button>
+	@role('Administrator|Agent')
+		<button class="delete-ticket btn btn-danger btn-sm" data-id="{{ $id }}"><i class="bi bi-trash"></i></button>
+	@endrole
 </div>
